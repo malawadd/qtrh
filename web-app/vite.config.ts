625b69440bbd9eb7469014e1ff3fdf8306858e69
0xgoldenlion <50057882+malawadd@users.noreply.github.com>
2023-05-16 14:44:28 +0300

    🏮 update config

--- web-app/vite.config.ts
+++ web-app/vite.config.ts
@@ -1,6 +1,30 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 
-export default defineConfig({
-	plugins: [sveltekit()]
+const config = defineConfig({
+  plugins: [sveltekit()],
+  test: {
+    // Jest like globals
+    globals: true,
+    environment: 'jsdom',
+    include: ['src/**/*.{test,spec}.ts'],
+    exclude: ['src/e2e-tests/.tmp/**'],
+    setupFiles: ['./setup-test.js'],
+    deps: {
+      inline: ['@ethersproject/signing-key', '@ethersproject/basex', '@depay/solana-web3.js'],
+    },
+  },
+  build: {
+    target: 'es2020',
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      target: 'es2020',
+    },
+  },
+  preview: {
+    host: '0.0.0.0',
+  },
 });
+
+export default config;
