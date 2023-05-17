c9c7dae2ee26e51169286fb86f3059e8e2c98fef
0xgoldenlion <50057882+malawadd@users.noreply.github.com>
2023-05-17 11:11:14 +0300

    🛰️  style comps

---
+++ web-app/src/lib/components/annotation-box/annotation-box.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+  import WarningIcon from 'design-system/icons/ExclamationCircle.svelte';
+  export let type: 'warning' | 'info' = 'warning';
+  export let size: 'normal' | 'small' = 'normal';
+</script>
+
+<div class="annotation-box typo-text-small {type} {size}">
+  <div class="content">
+    <WarningIcon
+      style="height: 1.25rem; width: 1.25rem; fill: var({type === 'info'
+        ? '--color-primary-level-6'
+        : '--color-caution-level-6'});"
+    />
+    <slot />
+  </div>
+</div>
+
+<style>
+  .annotation-box {
+    border-radius: 1rem 0 1rem 1rem;
+    padding: 0.75rem;
+    text-align: left;
+  }
+
+  .annotation-box > .content {
+    display: flex;
+    gap: 0.5rem;
+  }
+
+  .annotation-box.small {
+    border-radius: 0.75rem 0 0.75rem 0.75rem;
+    padding: 0.5rem;
+    background-color: red;
+    border-color: none;
+  }
+
+  .annotation-box.warning {
+    background-color: var(--color-caution-level-1);
+    border: 1px solid var(--color-caution-level-6);
+    color: var(--color-caution-level-6);
+  }
+
+  .annotation-box.info {
+    background-color: var(--color-primary-level-1);
+    border: 1px solid var(--color-primary-level-6);
+  }
+</style>
---
+++ web-app/src/lib/components/divider/divider.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  export let sideMargin = 0;
+</script>
+
+<div class="divider" style={`margin: 0.25rem ${sideMargin}rem`} />
+
+<style>
+  .divider {
+    height: 0.1rem;
+    border-radius: 0.25rem;
+    background-color: var(--color-foreground);
+  }
+</style>
