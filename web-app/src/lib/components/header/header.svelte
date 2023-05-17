1b2e2b508b3123b2ae89f488ba874718ecf17ba7
0xgoldenlion <50057882+malawadd@users.noreply.github.com>
2023-05-17 11:44:22 +0300

    😶‍🌫️ header done

---
+++ web-app/src/lib/components/header/header.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+  import scroll from '$lib/stores/scroll';
+  import wallet from '$lib/stores/wallet/wallet.store';
+  import BetaBadge from '../beta-badge/beta-badge.svelte';
+  import ConnectButton from '../connect-button/connect-button.svelte';
+  import SearchBar from '../search-bar/search-bar.svelte';
+  import Logo from './logo.svelte';
+
+  $: elevated = $scroll.pos > 16;
+</script>
+
+<header class:elevated>
+  <a href={$wallet.connected ? '/app/dashboard' : '/'}>
+    <Logo />
+    <div class="beta-badge">
+      <BetaBadge />
+    </div>
+  </a>
+  <SearchBar />
+  <div class="wallet">
+    <ConnectButton />
+  </div>
+</header>
+
+<style>
+  header {
+    height: 4rem;
+    width: 100%;
+    background-color: var(--color-background);
+    transition: box-shadow 0.3s, background-color 0.5s;
+    display: flex;
+    gap: 0.5rem;
+    align-items: center;
+    justify-content: space-between;
+    padding: 1rem;
+  }
+
+  a {
+    display: flex;
+    gap: 0.5rem;
+  }
+
+  header.elevated {
+    box-shadow: var(--elevation-low);
+  }
+
+  .wallet {
+    display: flex;
+    gap: 0.5rem;
+    align-items: center;
+  }
+
+  @media (max-width: 577px) {
+    .beta-badge {
+      display: none;
+    }
+  }
+</style>
